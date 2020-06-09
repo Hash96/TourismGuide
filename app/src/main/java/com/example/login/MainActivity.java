9106e57227a035427b8495cae887f7380f78cea5
Hash96 <hmhmkumara@students.nsbm.lk>
2020-06-09 07:22:57 +0530

    assinging buttons and values

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button a_button;
+    EditText a_username, a_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        a_button = findViewById(R.id.a_button);
+        a_username = findViewById(R.id.a_username);
+        a_password = findViewById(R.id.a_password);
+
+        a_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Login in", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
